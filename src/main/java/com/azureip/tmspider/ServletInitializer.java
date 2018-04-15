b4a3154caf57801271a452afd923206c7f3329fb
Lewis Zhang <fgwx@vip.qq.com>
2018-04-15 13:39:18 +0800

    Initial commit

---
+++ src/main/java/com/azureip/tmspider/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.azureip.tmspider;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TmspiderApplication.class);
+    }
+
+}
---
+++ src/test/java/com/azureip/tmspider/TmspiderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.azureip.tmspider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TmspiderApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
